4bd047e615788c32e29dd28b791b064dc8316a9d
sanghoon <psh950313@gmail.com>
2024-05-02 18:18:36 +0900

    한글 깨짐 방지 코드 삽입 후, @RequestMapping 사용법

--- Test/src/main/java/KOSA/spring/test/HomeController.java
+++ Test/src/main/java/KOSA/spring/test/HomeController.java
@@ -16,29 +16,78 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+
+private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+	
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+	
 		String formattedDate = dateFormat.format(date);
-		
-		
-		String helloWorld =  "Hello World - 박상훈";
-		model.addAttribute("helloworld", helloWorld);
+	
+		//추가한 부분
+		String helloWorld = "Hello World - 박상훈";
+		model.addAttribute("helloWorld",helloWorld);
 		
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+	
 		return "home";
 	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String add_form(Model model) {
+		return "add_form";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String add(int a, int b, Model model) {
 	
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+	
+		model.addAttribute("result", a + b);
+	
+		return "add";
+	}
+
+	@RequestMapping(value = "/add2Form")
+	public String addForm(Model model) {
+		return "add_form2";
+	}
+
+	@RequestMapping(value = "/add2")
+	public String add2(int a, int b, Model model) {
+	
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+	
+		model.addAttribute("result", a + b);
+	
+		return "add";
+	}
+
+	@RequestMapping(value = "/add3Form")
+	public String addForm3(Model model) {
+		return "add_form3";
+	}
+
+	@RequestMapping(value = "/add3")
+	public String add3(AddVO addVO, Model model) {
+	
+		System.out.println("a = " + addVO.getA());
+		System.out.println("b = " + addVO.getB());
+	
+		addVO.calc();
+	
+		model.addAttribute("result", addVO.getResult());
+		model.addAttribute("addVO", addVO);
+	
+		return "add3";
+	}
 }
--- Test/src/main/webapp/WEB-INF/views/home.jsp
+++ Test/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,15 +1,28 @@
+<%-- 한글 깨짐 방지 코드 삽입 --%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" %>
 <html>
 <head>
 	<title>Home</title>
+	
+	<%-- 한글 깨짐 방지 코드 삽입 --%>
+	<meta charset="UTF-8">
 </head>
 <body>
 <h1>
 	Hello world!  
 </h1>
 
+	<a href = "add">add</a><br/>
+	<a href = "add2Form">add2Form</a><br/>
+	<a href = "add3Form">add3Form</a><br/>
+	
 <P>  The time on the server is ${serverTime}. </P>
+
+
 <h1>${helloWorld}</h1>
 </body>
 </html>
