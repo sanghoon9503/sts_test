9cd9a1fe4eddc09bf183bdd3d0906f27d5fc13b1
sanghoon <psh950313@gmail.com>
2024-05-02 19:25:48 +0900

    로그 출력을 위한 @Slf4j 어노테이션 설정

--- Test/src/main/java/KOSA/spring/test/HomeController.java
+++ Test/src/main/java/KOSA/spring/test/HomeController.java
@@ -11,9 +11,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Handles requests for the application home page.
  */
+
 @Controller
 public class HomeController {
 
--- Test/src/main/java/KOSA/spring/test/SampleController.java
+++ Test/src/main/java/KOSA/spring/test/SampleController.java
@@ -1,5 +1,18 @@
 package KOSA.spring.test;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+//로그를 사용하기 위한 어노테이션
+@Slf4j
+@Controller
 public class SampleController {
 
+	
 }
